96d163358a0878b0b24e4dce698f6aa727ce3376
tulikaa25 <tulikaa257@gmail.com>
2025-10-24 12:05:53 +0530

    delete file

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-import app from './server/app.js';
-import 'dotenv/config'
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
